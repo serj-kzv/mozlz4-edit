9dca16515606ea0a4f6223b15323ff48298c63ce
serj.kzv <serj.kzv@gmail.com>
2018-05-02 20:02:03 +0300

    release

--- src/app/js/lib/app/mozLz4Archiver/command/MozLz4ArchiverCommandMozJSSLz4.js
+++ src/app/js/lib/app/mozLz4Archiver/command/MozLz4ArchiverCommandMozJSSLz4.js
@@ -1,7 +1,7 @@
 class MozLz4ArchiverCommandMozJSSLz4 extends MozLz4ArchiverCommand {
     constructor() {
         super({
-            TYPE_NAME: 'MozJSSLz4',
+            TYPE_NAME: 'mozJSSCLz40v001',
             HEADER: Uint8Array.from([109, 111, 122, 74, 83, 83, 67, 76, 122, 52, 48, 118, 48, 48, 49, 0]),
             DECOMPRESS_SIZE: Uint8Array.from([254, 254, 254, 127]),
             TRUNCATE_HEADER: true,
