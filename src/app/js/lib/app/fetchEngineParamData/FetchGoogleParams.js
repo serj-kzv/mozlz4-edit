51d74f5834e9b72c977dc62a2ac2aa2a8286fcb5
serj.kzv <serj.kzv@gmail.com>
2018-05-05 16:57:59 +0300

    dev

--- src/app/js/lib/app/fetchEngineParamData/FetchGoogleParams.js
+++ src/app/js/lib/app/fetchEngineParamData/FetchGoogleParams.js
@@ -1,6 +1,29 @@
 /**
  * https://developers.google.com/custom-search/docs/xml_results_appendices
  * https://developers.google.com/custom-search/json-api/v1/reference/cse/list
+
+ 0: "Boolean AND [.]"
+ ​
+ 1: "Boolean NOT [-]"
+ ​
+ 2: "Boolean OR [|]"
+ ​
+ 3: "Boolean Parentheses [()]"
+ ​
+ 4: "Hexadecimal\nEncoding"
+ ​
+ 5: "URL Escaped String"
+ ​
+ 6: "Associated Supported Languages"
+ ​
+ 7: "hl Parameter Value"
+ ​
+ 8: "lr Parameter Value"
+ ​
+ 9: "Country Collection Name"
+ ​
+ 10: "Country Code"
+
  */
 class FetchGoogleParams {
     static async fetch() {
@@ -8,57 +31,66 @@ class FetchGoogleParams {
         const engines = await (await fetch(url)).json();
         const type = engines.types.filter(type => type.name === 'General')[0];
         const engine = type.engines.filter(engine => engine.name === 'Google')[0];
-        let param = engine.params.filter(param => param.param === 'hl')[0];
+        const params = engine.params;
+        const paramNames = Object.keys(this.getConf());
+        // let param = engine.params.filter(param => param.param === 'hl')[0];
 
-        param.values = await FetchGoogleParams.fetchParams(FetchGoogleParams.getConf().webInterfaceLanguageCodes);
+        for (let paramName of paramNames) {
+            let param = FetchGoogleParams.getParamObj(params, paramName);
 
-        param = engine.params.filter(param => param.param === 'lr')[0];
-
-        param.values = await FetchGoogleParams.fetchParams(FetchGoogleParams.getConf().searchLanguageCodes);
+            param.values = await this.fetchParams(this.getConf()[`${paramName}`]);
+        }
 
         console.log(JSON.stringify(engines));
     }
 
+    static getParamObj(params, name) {
+        const param = params.filter(param => param.param === name)[0];
+
+        if (param.length === 0) {
+            param[`${name}`] = [];
+        }
+
+        return param;
+    }
+
     static async fetchParams(conf) {
+        const iframe = document.createElement("iframe");
+
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+
         const html = await (await fetch(conf.url)).text();
-        const div = document.createElement("div");
 
-        div.style.display = 'none';
-        document.body.appendChild(div);
-        div.innerHTML = html;
+        iframe.contentDocument.write(html);
 
-        const params = await this.findParams(div, conf);
+        const params = this.findParams(iframe.contentDocument, conf);
 
-        // iframe.remove();
+        iframe.remove();
 
         return params;
     }
 
-    static async findParams(div, conf) {
-        console.log(div)
-        const trs = Array.from(div.querySelectorAll(conf.selector));
-        console.log(trs)
-        const params = trs.map(tr => {
+    static findParams(doc, conf) {
+        return Array.from(doc.querySelectorAll(conf.selector)).map(tr => {
             return {
                 name: tr.children[0].innerText.trim(),
                 value: tr.children[1].innerText.trim()
             }
         });
-
-        console.log(params)
-
-        return params;
     }
 
     static getConf() {
         return Object.freeze({
-            webInterfaceLanguageCodes: {
+            // Supported Interface Languages
+            hl: {
                 url: 'https://developers.google.com/custom-search/docs/xml_results_appendices',
-                selector: 'div.devsite-table-wrapper:nth-child(53) > table:nth-child(1) > tbody:nth-child(1) > tr:not(:first-child)'
+                selector: 'table:nth-child(53) tr:not(:first-child)'
             },
-            searchLanguageCodes: {
+            // Language Collection Values
+            lr: {
                 url: 'https://developers.google.com/custom-search/docs/xml_results_appendices',
-                selector: 'div.devsite-table-wrapper:nth-child(57) > table:nth-child(1) > tbody:nth-child(1) > tr:not(:first-child)'
+                selector: 'table:nth-child(57) tr:not(:first-child)'
             }
         });
     }
