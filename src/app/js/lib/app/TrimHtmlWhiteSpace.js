66b9f5ce731b38634833773c104b210872b87a02
serj.kzv <serj.kzv@gmail.com>
2018-06-11 17:47:40 +0300

    dev

--- src/app/js/lib/app/TrimHtmlWhiteSpace.js
+++ src/app/js/lib/app/TrimHtmlWhiteSpace.js
@@ -1,10 +1,11 @@
 class TrimHtmlWhiteSpace {
     static trim(parent) {
-        const children = Array.from(parent.childNodes);
-        const nodes = children.filter(node => node.nodeType !== 3);
-        const textNodes = children.filter(node => node.nodeType === 3 && !/\S/.test(node.nodeValue));
-
-        textNodes.forEach(node => parent.replaceChild(document.createTextNode(''), node));
-        nodes.forEach(node => TrimHtmlWhiteSpace.trim(node));
+        Array.from(parent.childNodes).forEach(node => {
+            if (node.nodeType === 3 && !/\S/.test(node.nodeValue)) {
+                parent.replaceChild(document.createTextNode(''), node);
+            } else if (node.nodeType !== 3) {
+                TrimHtmlWhiteSpace.trim(node);
+            }
+        });
     }
 }
