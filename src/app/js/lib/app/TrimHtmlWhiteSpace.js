bde69df73bb0852a300b94dcbb1626236125c28f
null <null>
2018-09-27 20:27:03 +0300

    dev

--- src/app/js/lib/app/TrimHtmlWhiteSpace.js
+++ src/app/js/lib/app/TrimHtmlWhiteSpace.js
@@ -1,7 +1,9 @@
 class TrimHtmlWhiteSpace {
     static trim(parent) {
+        const rgEx = /\S/;
+
         Array.from(parent.childNodes).forEach(node => {
-            if (node.nodeType === 3 && !/\S/.test(node.nodeValue)) {
+            if (node.nodeType === 3 && !rgEx.test(node.nodeValue)) {
                 parent.replaceChild(document.createTextNode(''), node);
             } else if (node.nodeType !== 3) {
                 TrimHtmlWhiteSpace.trim(node);
