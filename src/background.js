6a9114494a742c7ca944c6ab6e979a3a6c5aba88
null <null>
2022-10-02 15:20:52 +0300

    null

--- src/background.js
+++ src/background.js
@@ -1,7 +1,1 @@
-browser.browserAction.onClicked.addListener(
-	async tab => {
-	  browser.tabs.create({
-		url: '/index.html'
-	  });
-	}
-);
+browser.browserAction.onClicked.addListener(async () => browser.tabs.create({url: '/index.html'}));
